d43342a05522e3164032b238e497ba89d8c52081
Ella <ellaoyins@gmail.com>
2025-12-14 22:26:56 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+AOS.init();
+//  import { CountUp } from './js/countUp.min.js'; 
+
+const sidenavOpen = document.getElementById('sidenavOpen'); 
+const sidenavClose = document.getElementById('sidenavClose');
+const sidenav = document.getElementById('sidenav');
+
+// Side navigation
+sidenavOpen.style.display = 'block';
+sidenavClose.style.display = 'none'; 
+
+function showSidenav(){
+  const sidenav = document.querySelector('.sidenav')
+   sidenavOpen.style.display = 'none'; 
+   sidenavClose.style.display = 'block';
+  sidenav.style.display = 'flex' 
+}
+
+function hideSidenav(){
+  const sidenav = document.querySelector('.sidenav')
+   sidenavOpen.style.display = 'block'; 
+  sidenavClose.style.display = 'none';
+  sidenav.style.display = 'none' 
+} 
+
+// Back to top button
+const mybutton = document.getElementById("backToTopBtn"); 
+
+window.onscroll = function() {scrollFunction}; 
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+mybutton.addEventListener("click", topFunction);
+
+function topFunction() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+}
+
+
+const counter = new CountUp('counter', 24); 
+if (!counter.error) {
+  counter.start(); 
+} else {
+  console.error(counter.error); 
+}
